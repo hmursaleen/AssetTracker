b377b2e2101720e4b04c51805f4b88b2ab0f0d82
hmursaleen <habibulmursaleen@gmail.com>
2024-01-28 16:23:55 +0600

    added api documentation link http://localhost:8000/swagger/

--- asset_management/urls.py
+++ asset_management/urls.py
@@ -3,6 +3,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
+from rest_framework.routers import DefaultRouter
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
--- asset_tracking/urls.py
+++ asset_tracking/urls.py
@@ -6,4 +6,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('asset_management.urls')),
+    path('', include('asset_management.urls')),
 ]
